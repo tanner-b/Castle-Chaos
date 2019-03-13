0be6f97ac33b2f7b244abae4a718fa9f8cb78049
Sofia Ilina <sofia.ilina@mail.utoronto.ca>
2019-03-13 23:06:00 +0100

    compiled version of Draw_Grid and Draw_Select FSMs in CastleChaos.v

--- CastleChaos.v
+++ CastleChaos.v
@@ -32,23 +32,12 @@ module CastleChaos
 	
 	wire resetn;
 	assign resetn = SW[17];
-	
-	
 											//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~??
 	
-	// Create the colour, x, y and writeEn wires that are inputs to the controller.
+	// wires for passing in the coordinates and colour of the pixel to be drawn
 	wire [2:0] colour;
 	wire [7:0] x;
 	wire [6:0] y;
-	wire writeEn;
-	
-	wire load_x;
-	wire load_y;
-	
-	// Wire to draw a single white box on the board. We will need 8 of these blocks
-	// each white block is 25x25 pixels, so we need 6 bits.
-	wire [5:0] pixel_offsets;
-	
 
 	// Create an Instance of a VGA controller - there can be only one!
 	// Define the number of colours as well as the initial background
@@ -74,192 +63,208 @@ module CastleChaos
 		defparam VGA.BITS_PER_COLOUR_CHANNEL = 1;
 		defparam VGA.BACKGROUND_IMAGE = "black.mif";
 			
-	// Put your code here. Your code should produce signals x,y,colour and writeEn/plot
-	// for the VGA controller, in addition to any other functionality your design may require.
-    
+endmodule
 	 
 	 
 	 
-	 
-	 
-	 
-	 											//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~??
+
+// each checkerboard grid consists of 2 regions:
+//	- the inner box which can either contain a blue or yellow figure (fg)
+//		- if there is no figure on that grid, this colour will either be black or white, depending on its location
+// 	- the outer rim which surrounds the figure. This part can only be black or white, depenging on the grid'd location (bg)
+
+//	+---------------+
+//	|	bg			|		bg options: black, white
+//	|	+-------+	|
+//	|	|	fg	|	|		fg options: black, white, blue, yellow
+//	|	+-------+	|
+//	|				|
+//	+---------------+
+
+module Draw_Grid_FSM(clk, done, draw, reset, start_x, start_y, bg_colour, fg_colour, x_pos, y_pos, colour_out);
+// this module can draw a box of any size based on input. Largest box we may need is 25x25,
+// so the pixel counter size never needs more than 6 bits
+
+	input clk;
+	input draw;
+	input reset;
+
+	input [7:0] start_x;
+	input [6:0] start_y;
+	input [2:0] bg_colour;
+	input [2:0] fg_colour;
+
+	// this is passed into the module which draws the pixel on the screen
+	output reg [7:0] x_pos;
+	output reg [6:0] y_pos;
+	output reg [2:0] colour_out;
+
+	// this signals when the module finished drawing the current grid to the screen
+	output reg done = 1'b0;
+
+	// registers we will use for internal computations:
+	reg [4:0] x_incr;
+	reg [4:0] y_incr;
+	
+	reg curr_state = WAIT;
+	reg next_state = WAIT;
+	reg wait_one_cycle;
+	parameter WAIT = 2'b00, INCREMENT = 2'b01, DONE = 2'b11;
+
+	// this determines the next state
+	always@(*) begin
+		case(curr_state)
+			WAIT: next_state = draw ? INCREMENT : WAIT;
+			// if both x and y counter reached 0, we are done drawing the grid
+			INCREMENT: next_state = ((x_incr == 5'b00000) && (y_incr == 5'b00000)) ? DONE : INCREMENT;
+			DONE: next_state = WAIT;
+		endcase
+	end
+
+	// what to do at each state
+	always@(posedge clk) begin: state_table
+		case (curr_state) 
 		
-  
-	 //instantiate blank checker board
-	 DrawBoard board();
-	 DrawGrid box();
-	 
-	 
+			WAIT: begin 
+				done <= 1'b0;
+				// each counter starts at 25: each grid is 25x25 pixels
+				x_incr <= 5'b11001;
+				y_incr <= 5'b11001;
+			end
+
+			INCREMENT: begin
+				done <= 1'b0;
+				x_pos <= start_x + {5'b00000, x_incr[4:0]};
+				y_pos <= start_y + {4'b0000, y_incr[4:0]}; 
+
+				if (wait_one_cycle == 1'b1) wait_one_cycle = 1'b0;
+				else begin
+					// go through first row of x, then second row, ... etc
+					if (x_incr == 5'b00000) begin
+						x_incr <= 5'b11001;
+						y_incr <= y_incr - 5'b0001;
+					end else x_incr <= x_incr - 5'b0001;
+				end
+
+				// logic for colour_out: if 5<= x,y <= 19 => drawing fg, else drawing bg
+				if ((x_incr >= 5'b00101) && (x_incr <= 5'b10011) && (y_incr >= 5'b00101) && (y_incr <= 5'b10011))
+					colour_out <= fg_colour;
+				else colour_out <= bg_colour;
+			end
+
+			DONE: done <= 1'b1;
+
+		endcase
+	end
+
+	always @(posedge clk) begin	
+		curr_state = next_state;
+   end
+
 endmodule
 
 
-module DrawGrid();
-// this module can draw a box of any size based on input. Largest box we may need is 25x25,
-// so the pixel counter size never needs more than 6 bits
-	
-	input [2:0] colour_in;
-	input [5:0] pixel_counter;
+
+
+// selector will draw inside of a given grid. The thickness of the boarder will be one pixel wide
+
+module Selector_Drawer_FSM(clk, reset, draw, start_x, start_y, colour_in, done, x_pos, y_pos, colour_out);
+
 	input clk;
-	input reset_n;
+	input reset;
+	input draw;
 	input [7:0] start_x;
 	input [6:0] start_y;
-	
+	input [2:0] colour_in;
+
+	// this is passed into the module which draws the pixel on the screen
 	output reg [7:0] x_pos;
 	output reg [6:0] y_pos;
 	output [2:0] colour_out;
-	
 	assign colour_out = colour_in;
+
+
+	// this signals when the module finished drawing the current grid to the screen
+	output reg done = 1'b0;
 	
-	reg [7:0] reg_x; 
-	reg [6:0] reg_y;
+	// We need the same registers as in Draw_Grid_FSM for internal computations
+	reg [4:0] x_incr;
+	reg [4:0] y_incr;
 	
-	always@(posedge clk) begin
-		if (!reset_n) begin
-			x_pos <= 8'b00000000;
-			y_pos <= 7'b0000000;
-			x_pos <= start_x + {5'b00000, pixel_counter[2:0]}; // We use pixel_counter[2:0] as x offset
-			y_pos <= start_y + {4'b0000, pixel_counter[5:3]}; // We use pixel_counter[5:3] as y offset
-		end
+	reg curr_state = WAIT;
+	reg next_state = WAIT;
+	reg wait_one_cycle;
+	parameter WAIT = 3'b000, BOTTOM_BOARDER = 3'b001, LEFT_BOARDER = 3'b011, TOP_BOARDER = 3'b111, RIGHT_BOARDER = 3'b110, DONE = 3'b100;
+
+	// this determines the next state
+	always@(*) begin
+		case(curr_state)
+			WAIT: next_state = draw ? TOP_BOARDER : WAIT;
+			// X counter reached 0, Y stayed at 25
+			BOTTOM_BOARDER: next_state = ((x_incr == 5'b00000) && (y_incr == 5'b11001)) ? RIGHT_BOARDER : TOP_BOARDER;
+			// Y counter reached 0, X stayed at 0
+			LEFT_BOARDER: next_state = ((x_incr == 5'b00000) && (y_incr == 5'b00000)) ? BOTTOM_BOARDER : RIGHT_BOARDER;
+			// X counter went back up to 25, Y stayed at 0
+			TOP_BOARDER: next_state = ((x_incr == 5'b11001) && (y_incr == 5'b00000)) ? LEFT_BOARDER : BOTTOM_BOARDER;
+			// Y counter went back up to 25, X stayed at 25
+			RIGHT_BOARDER: next_state = ((x_incr == 5'b11001) && (y_incr == 5'b11001)) ? DONE : LEFT_BOARDER;
+			DONE: next_state = WAIT;
+		endcase
+	end
+	
+	
+	always @(posedge clk) begin	
+		curr_state = next_state;
+   end
+
+
+   // what to do at each state
+	always@(posedge clk) begin: state_table
+		case (curr_state) 
+		
+			WAIT: begin 
+				done <= 1'b0;
+				// each counter starts at 25: boarder is drawn on the interior pixel of a single grid
+				x_incr <= 5'b11001;
+				y_incr <= 5'b11001;
+			end
+
+			BOTTOM_BOARDER: begin
+				x_pos <= start_x + {5'b00000, x_incr[4:0]};
+				y_pos <= start_y + {4'b0000, y_incr[4:0]}; 
+				if (wait_one_cycle == 1'b1) wait_one_cycle = 1'b0;
+				else x_incr <= x_incr - 5'b0001;
+			end
+
+			LEFT_BOARDER: begin
+				x_pos <= start_x + {5'b00000, x_incr[4:0]};
+				y_pos <= start_y + {4'b0000, y_incr[4:0]}; 
+				if (wait_one_cycle == 1'b1) wait_one_cycle = 1'b0;
+				else y_incr <= y_incr - 5'b0001;
+			end
+
+			TOP_BOARDER: begin
+				x_pos <= start_x + {5'b00000, x_incr[4:0]};
+				y_pos <= start_y + {4'b0000, y_incr[4:0]}; 
+				if (wait_one_cycle == 1'b1) wait_one_cycle = 1'b0;
+				else x_incr <= x_incr + 5'b0001;
+			end
+
+			RIGHT_BOARDER: begin
+				x_pos <= start_x + {5'b00000, x_incr[4:0]};
+				y_pos <= start_y + {4'b0000, y_incr[4:0]}; 
+				if (wait_one_cycle == 1'b1) wait_one_cycle = 1'b0;
+				else y_incr <= y_incr + 5'b0001;
+			end
+
+			DONE: next_state = WAIT;
+		endcase
+		
+	end
+		
 endmodule
 
-
-
-module DrawBoard();	
 
 	// locations of white grid boards (x,y):
 	//		([56:80], [11:35]);		([106:130], [11:35]);		([31:55], [36:60]);		([81:105], [36:60]);
 	//		([56:80], [61:85]);		([106:130], [61:85]);		([31:55], [86:110]);	([81:105], [86:110]);
-
-endmodule
-
-
-
-
-
-
-
-module data_path(x_pos, y_pos, colour_out, clk, colour_in, load_val, load_x, load_y, pixel_counter, reset_n);
-
-	input [2:0] colour_in;
-	input [6:0] load_val;
-	input load_x; 				// From FSM
-	input load_y;				// From FSM
-	input [5:0] pixel_counter;	// From FSM - Bits 0 and 1 are x offset, bits 2 and 3 are y offset
-	input reset_n;
-	input clk;
-	
-	output reg [7:0] x_pos;
-	output reg [6:0] y_pos;
-	output [2:0] colour_out;
-	
-	assign colour_out = colour_in;
-	
-	reg [7:0] reg_x; 
-	reg [6:0] reg_y;
-	
-	always@(posedge clk) begin
-		if (!reset_n) begin
-			reg_x <= 8'b00000000;
-			reg_y <= 7'b0000000;
-			x_pos <= 8'b00000000;
-			y_pos <= 7'b0000000;
-		end
-		
-		if (load_x) // We want to load the x register
-			reg_x <= {1'b0, load_val}; // MSB is 0, we will only be able to access 128 of the 160 cols
-		
-		if (load_y) // We want to load the y register
-			reg_y <= load_val; // MSB is 0, the display is limited to 120 rows apparently
-		
-		if (!load_x & !load_y) begin // We want to output some pixel offset to the pixel registers
-			x_pos <= reg_x + {6'b000000, pixel_counter[1:0]}; // We use pixel_counter[1:0] as x offset
-			y_pos <= reg_y + {5'b00000, pixel_counter[3:2]}; // We use pixel_counter[3:2] as y offset
-		end
-	end
-
-endmodule
-
-module FSM(go, ResetN, LoadX, LoadY, increment, draw, clk);
-	input go;
-	input ResetN;
-	input clk;
-	
-	output reg LoadX;
-	output reg LoadY;
-	output reg [5:0] increment;
-	output reg draw;
-	
-	reg [2:0] curr_state = WAIT_LOAD_X;
-	reg [2:0] next_state = WAIT_LOAD_X;
-	
-	parameter WAIT_LOAD_X = 3'b000, LOAD_X = 3'b001, WAIT_LOAD_Y = 3'b010, LOAD_Y = 3'b011, INCREMENT = 3'b100;
-	reg wait_one_cycle = 1'b0;
-	
-	always@(*) begin
-		case (curr_state)
-			WAIT_LOAD_X: next_state = go ? LOAD_X : WAIT_LOAD_X; 
-			LOAD_X: next_state = go ? LOAD_X : WAIT_LOAD_Y; 
-			WAIT_LOAD_Y: next_state = go ? LOAD_Y : WAIT_LOAD_Y;
-			LOAD_Y: next_state = go ? LOAD_Y : INCREMENT;
-			INCREMENT: next_state = increment == 4'b1111 ? WAIT_LOAD_X : INCREMENT;
-		endcase
-		
-		if(ResetN == 1'b0)
-			next_state = WAIT_LOAD_X; // Should restart at getting X
-	end
-	
-	always@(posedge clk)
-	begin: state_table
-		case (curr_state)	
-		
-			INCREMENT: begin
-				LoadX <= 0;
-				LoadY <= 0;
-				if (wait_one_cycle == 1'b1)
-				begin
-					wait_one_cycle = 1'b0;
-					draw <= 0;
-					end
-				else
-					begin
-					increment <= increment + 4'b0001;
-					draw <= 1;
-					end
-					
-			end
-			
-			LOAD_Y: begin
-				LoadX <= 0;
-				LoadY <= 1;
-				draw <= 0;
-				increment <= 4'b0000;
-				wait_one_cycle = 1'b1;
-			end
-			
-			WAIT_LOAD_Y: begin
-				LoadX <= 0;
-				LoadY <= 0;
-				draw <= 0;
-			end	
-
-			LOAD_X: begin
-				LoadX <= 1;
-				LoadY <= 0;
-				draw <= 0;
-			end
-
-			WAIT_LOAD_X: begin
-				LoadX <= 0;
-				LoadY <= 0;
-				draw <= 0;
-			end			
-				
-		endcase
-	end
-	
-	always @(posedge clk) begin
-		
-		curr_state = next_state;
-   end	
-endmodule
